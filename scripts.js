36e5e5e59bed3ec8d9971edb370ba2be148ac9ba
Andy Conn <andyconn1988@googlemail.com>
2020-11-29 16:41:34 +0000

    update to button code

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
 document.getElementById("change_text").innerHTML = "Code works!";
+
+
+document.getElementById("test_button").onclick = function() {testFunction()};
+
+function testFunction() {
+  document.getElementById("change_text").innerHTML = "Button Clicked!";
+}
