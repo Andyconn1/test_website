eb4d769842d993eb90a8dcb66946bf93cce888f1
Andy Conn <andyconn1988@googlemail.com>
2020-11-29 17:03:15 +0000

    fix attempt

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
-//document.getElementById("change_text").innerHTML = "Code works!";
+// Set Version
+document.getElementById("Version").innerHTML = "Version: 2";
 
-
+// Assign function to button
 document.getElementById("test_button").onclick = function() {testFunction()};
 
+// Define test function 
 function testFunction() {
     document.getElementById("change_text").innerHTML = "Button Clicked!";
     alert("test");
