d552db2ca1b801dcfa00c82a00fc03d04593d14d
Andy Conn <andyconn1988@googlemail.com>
2020-11-29 16:51:45 +0000

    change

--- scripts.js
+++ scripts.js
@@ -4,5 +4,6 @@ document.getElementById("change_text").innerHTML = "Code works!";
 document.getElementById("test_button").onclick = function() {testFunction()};
 
 function testFunction() {
-  document.getElementById("change_text").innerHTML = "Button Clicked!";
+  // document.getElementById("change_text").innerHTML = "Button Clicked!";
+    alert("test");
 }
